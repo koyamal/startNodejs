9eef976484d5d754c80714c768b2f0bdf4c07c75
koyamal <tanakamaruko1121n@gmail.com>
2023-03-11 12:49:19 +0900

    create debug directory

---
+++ debug/index.js
@@ -0,0 +1,13 @@
+var parent = function () {
+  console.log("parent() is called.");
+  child();
+  console.log("parent() is executed.");
+};
+
+var child = function () {
+  console.log("child() is called.");
+  console.log("child() is executed.");
+  console.log('bye');
+};
+
+parent();
