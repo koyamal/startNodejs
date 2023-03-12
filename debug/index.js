8ca3c396c65ce06ef94f9b5db34ae814aad6516e
koyamal <tanakamaruko1121n@gmail.com>
2023-03-12 10:10:07 +0900

    Change var to let

--- debug/index.js
+++ debug/index.js
@@ -1,13 +1,18 @@
-var parent = function () {
+let parent = function () {
   console.log("parent() is called.");
   child();
   console.log("parent() is executed.");
 };
 
-var child = function () {
+let child = function () {
   console.log("child() is called.");
+  child_child();
   console.log("child() is executed.");
   console.log('bye');
 };
+
+let child_child = function(){
+  console.log("child_child is called");
+}
 
 parent();
