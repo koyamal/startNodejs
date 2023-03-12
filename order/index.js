39e0ce549cf680117c032bc00a453d72009947ec
koyamal <tanakamaruko1121n@gmail.com>
2023-03-12 11:21:42 +0900

    Check order of delay function

---
+++ order/index.js
@@ -0,0 +1,15 @@
+setTimeout(function(){
+    console.log('setTimeout');
+}, 100);
+
+setImmediate(function(){
+    console.log('setImmediate');
+})
+
+process.nextTick(function(){
+    console.log('nextTick');
+})
+
+Promise.resolve().then(function(){
+    console.log('Promise');
+})
