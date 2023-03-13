77f9d14a7ecf1ce13f82a0376d998898347eb658
koyamal <tanakamaruko1121n@gmail.com>
2023-03-13 11:38:20 +0900

    Check allow function

--- event/checkallowfunction.js
+++ event/checkallowfunction.js
@@ -5,7 +5,7 @@ const person = {
     }
 }
 
-person.hello();
+// person.hello();
 
 const person2 = {
     name: 'Tom',
@@ -14,4 +14,34 @@ const person2 = {
     }
 }
 
-person2.hello();
+// person2.hello();
+
+const a = () => {
+    console.log('A is called ', this.name);
+}
+
+const person3 = {
+    name: 'Tom',
+    hello: function(){
+        console.log('Hello ' + this.name);
+        a();
+        const b = () => {
+            console.log('B is called ', this.name);
+        };
+        b();
+    }
+}
+
+person3.hello();
+
+function c() {
+    const d = () => console.log('D: ', this.name);
+    const e = function(){
+        console.log('E: ', this.name);
+    }
+
+    d();
+    e();
+}
+
+c();
