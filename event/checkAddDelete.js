e2e21698d04d08a384a6f8ac7de7358be24b021e
koyamal <tanakamaruko1121n@gmail.com>
2023-03-13 14:59:38 +0900

    Add and Delete listener

---
+++ event/checkAddDelete.js
@@ -0,0 +1,30 @@
+const { EventEmitter } = require("events");
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('box', () => {
+  console.log('listener 0');
+  console.log('Sum of this listener is ' + eventEmitter.listenerCount('box'));
+});;
+
+const listenerMessage1 = () => console.log('This is listener1');
+const listenerMessage2 = () => console.log('This is listener2');
+
+eventEmitter.on('addItem1', () => {
+  console.log('add listener1');
+  eventEmitter.addListener('box', listenerMessage1);
+});
+
+eventEmitter.on('addItem2', () => {
+  console.log('add listener2');
+  eventEmitter.addListener('box', listenerMessage2);
+});
+
+eventEmitter.on('removeItem', () => {
+  console.log('remove listener');
+  eventEmitter.removeListener('box', listenerMessage1);
+});
+
+eventEmitter.emit('addItem1');
+eventEmitter.emit('addItem2');
+eventEmitter.emit('removeItem');
+eventEmitter.emit('box');
