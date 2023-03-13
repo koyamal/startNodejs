8ba9486a35d212f97e7eefb9f666b0d5c05f363c
koyamal <tanakamaruko1121n@gmail.com>
2023-03-13 11:58:46 +0900

    Check arrow function

--- event_setting/index.js
+++ event_setting/index.js
@@ -2,13 +2,21 @@ const EventEmitter = require('events');
 
 const ee = new EventEmitter();
 
-const ontick = function(){
-    console.log('event is called');
-    // ee.off('event', ontick);
-};
+// const ontick = function(){
+//     console.log('event is called');
+//     // ee.off('event', ontick);
+// };
 
 // ee.on('event', ontick);
-ee.once('event', ontick);
+// ee.once('event', ontick);
+
+ee.on('event', function(){
+    console.log("function: ", this);
+});
+
+ee.on('event', () => {
+    console.log("arrow: ", this);
+});
 
 ee.emit('event');
 ee.emit('event');
