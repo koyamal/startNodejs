efa36807c250c30d64a1998f698ee0bba1c9a5e1
koyamal <tanakamaruko1121n@gmail.com>
2023-03-15 18:01:53 +0900

    Check type file

--- path/index.js
+++ path/index.js
@@ -4,3 +4,4 @@ const filepath = '/Users/mike/work/temp/index.js';
 
 console.log('dirname: ', path.dirname(filepath));
 console.log('basename: ', path.basename(filepath));
+console.log('extname: ', path.extname(filepath));
