a51072dd665bd6044d22f1f8f33c259259ded819
koyamal <tanakamaruko1121n@gmail.com>
2023-03-15 18:03:41 +0900

    Use join

--- path/index.js
+++ path/index.js
@@ -5,3 +5,7 @@ const filepath = '/Users/mike/work/temp/index.js';
 console.log('dirname: ', path.dirname(filepath));
 console.log('basename: ', path.basename(filepath));
 console.log('extname: ', path.extname(filepath));
+
+
+const filepath2 = path.join('/Users/mike','temp/index.js');
+console.log('join: ', filepath2);
