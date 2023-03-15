ecba5d971791d2b4a548dc869f9f3e7d6445f28f
koyamal <tanakamaruko1121n@gmail.com>
2023-03-15 18:01:07 +0900

    Use path

---
+++ path/index.js
@@ -0,0 +1,6 @@
+const path = require('path');
+
+const filepath = '/Users/mike/work/temp/index.js';
+
+console.log('dirname: ', path.dirname(filepath));
+console.log('basename: ', path.basename(filepath));
