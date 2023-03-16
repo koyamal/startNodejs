dcca242aa25354f06c22a31b9b71176e7755e1fa
koyamal <tanakamaruko1121n@gmail.com>
2023-03-16 18:31:27 +0900

    Log data

--- file_sync/index.js
+++ file_sync/index.js
@@ -6,3 +6,5 @@ const data = fs.readFileSync(path.join(__dirname, 'sample.txt'), 'utf-8');
 // console.log(data);
 
 fs.writeFileSync(path.join(__dirname, 'sample-copy.txt'), data, 'utf-8');
+
+console.log(data);
