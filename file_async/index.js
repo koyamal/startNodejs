4d27ea393b5a37891dd6d424ec28a798acd1f28b
koyamal <tanakamaruko1121n@gmail.com>
2023-03-18 11:45:10 +0900

    Use util.promisify

--- file_async/index.js
+++ file_async/index.js
@@ -1,18 +1,21 @@
 const fs = require('fs');
 const path = require('path');
+const util = require('util');
 
-fs.readFile(path.join(__dirname, 'sample.txt'), 'utf-8', function(err, data){
-    if(err){
-        console.log(err.message);
-        return
-    }
-    // console.log(data);
+const readFile = util.promisify(fs.readFile);
 
-    fs.writeFile(path.join(__dirname, 'sample-copy.txt'), data, 'utf-8', function(err, data){
-        if(err){
-            console.log(err.message);
-            return
-        }
-        console.log('OK');
-    })
-});
+// fs.readFile(path.join(__dirname, 'sample.txt'), 'utf-8', function(err, data){
+//     if(err){
+//         console.log(err.message);
+//         return
+//     }
+//     // console.log(data);
+
+//     fs.writeFile(path.join(__dirname, 'sample-copy.txt'), data, 'utf-8', function(err, data){
+//         if(err){
+//             console.log(err.message);
+//             return
+//         }
+//         console.log('OK');
+//     })
+// });
