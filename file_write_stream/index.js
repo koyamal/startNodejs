729db2f0ea744dc761ed3f16fb828d1ab0703887
koyamal <tanakamaruko1121n@gmail.com>
2023-03-18 17:55:47 +0900

    Read/Write stream

--- file_write_stream/index.js
+++ file_write_stream/index.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 
-const writer = fs.createWriteStream(path.join(__dirname, 'create.txt'), 'utf-8');
-writer.end("Hello World !");
+const reader = fs.createReadStream(path.join(__dirname, 'sample.txt'), 'utf-8');
+const writer = fs.createWriteStream(path.join(__dirname, 'sample-cp.txt'), 'utf-8');
+
+reader.pipe(writer);
+reader.resume();
