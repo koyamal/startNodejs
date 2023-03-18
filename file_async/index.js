5c867686245d817e35247eab3a62a4c06cafa28f
koyamal <tanakamaruko1121n@gmail.com>
2023-03-18 11:43:35 +0900

    Write data

--- file_async/index.js
+++ file_async/index.js
@@ -6,5 +6,13 @@ fs.readFile(path.join(__dirname, 'sample.txt'), 'utf-8', function(err, data){
         console.log(err.message);
         return
     }
-    console.log(data);
+    // console.log(data);
+
+    fs.writeFile(path.join(__dirname, 'sample-copy.txt'), data, 'utf-8', function(err, data){
+        if(err){
+            console.log(err.message);
+            return
+        }
+        console.log('OK');
+    })
 });
