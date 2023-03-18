b7bd0f101f687482577f81be354a5c5bece4ad3f
koyamal <tanakamaruko1121n@gmail.com>
2023-03-18 17:42:25 +0900

    Use async/await

--- file_async/index.js
+++ file_async/index.js
@@ -5,14 +5,26 @@ const util = require('util');
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
-readFile(path.join(__dirname, 'sample.txt'), 'utf-8')
-.then((data) =>{
-    return writeFile(path.join(__dirname, 'sample-cp.txt'), data, 'utf-8');
-}).then(()=>{
-    console.log('OK');
-}).catch((err) =>{
-    console.log(err.message);
-})
+const fn = async function(){
+    try{
+        const data = await readFile(path.join(__dirname, 'sample.txt'), 'utf-8');
+        await writeFile(path.join(__dirname, 'sample-cp02.txt'), data, 'utf-8');
+        console.log('OK');
+    }catch(err){
+        console.log(err.message);
+    }
+}
+
+fn();
+
+// readFile(path.join(__dirname, 'sample.txt'), 'utf-8')
+// .then((data) =>{
+//     return writeFile(path.join(__dirname, 'sample-cp.txt'), data, 'utf-8');
+// }).then(()=>{
+//     console.log('OK');
+// }).catch((err) =>{
+//     console.log(err.message);
+// })
 
 // fs.readFile(path.join(__dirname, 'sample.txt'), 'utf-8', function(err, data){
 //     if(err){
