28b40c99dfdbeeffa634a188b4d82aa2a133f1ff
koyamal <tanakamaruko1121n@gmail.com>
2023-03-18 17:38:23 +0900

    Use promise

--- file_async/index.js
+++ file_async/index.js
@@ -3,6 +3,16 @@ const path = require('path');
 const util = require('util');
 
 const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+readFile(path.join(__dirname, 'sample.txt'), 'utf-8')
+.then((data) =>{
+    return writeFile(path.join(__dirname, 'sample-cp.txt'), data, 'utf-8');
+}).then(()=>{
+    console.log('OK');
+}).catch((err) =>{
+    console.log(err.message);
+})
 
 // fs.readFile(path.join(__dirname, 'sample.txt'), 'utf-8', function(err, data){
 //     if(err){
